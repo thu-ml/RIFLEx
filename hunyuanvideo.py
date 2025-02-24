5815e93214503f917e3244b6f1ace915ad9be3db
gracezhao1997 <gracezhao1997@gmail.com>
2025-02-24 21:24:09 +0800

    update

--- hunyuanvideo.py
+++ hunyuanvideo.py
@@ -46,11 +46,11 @@ class HunyuanVideoRotaryPosEmbedRifleX(HunyuanVideoRotaryPosEmbed):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--k', type=int, help='Index of intrinsic frequency', default=4)
-    parser.add_argument('--N_k', type=int, help='the period of intrinsic frequency', default=50)
+    parser.add_argument('--N_k', type=int, help='The period of intrinsic frequency in latent space', default=50)
     parser.add_argument('--num_frames', type=int, help='Number of frames for inference', default=261)
-    parser.add_argument('--finetune', help='whether finetuned version', action='store_true')
+    parser.add_argument('--finetune', help='Whether finetuned version', action='store_true')
     parser.add_argument('--model_id', type=str, help='huggingface path for models', default="hunyuanvideo-community/HunyuanVideo")
-    parser.add_argument('--prompt', type=str, help='prompt for generation',default="3D animation of a small, round, fluffy creature with big, expressive eyes explores a vibrant, enchanted forest. The creature, a whimsical blend of a rabbit and a squirrel, has soft blue fur and a bushy, striped tail. It hops along a sparkling stream, its eyes wide with wonder. The forest is alive with magical elements: flowers that glow and change colors, trees with leaves in shades of purple and silver, and small floating lights that resemble fireflies. The creature stops to interact playfully with a group of tiny, fairy-like beings dancing around a mushroom ring. The creature looks up in awe at a large, glowing tree that seems to be the heart of the forest.")
+    parser.add_argument('--prompt', type=str, help='Prompts for generation',default="3D animation of a small, round, fluffy creature with big, expressive eyes explores a vibrant, enchanted forest. The creature, a whimsical blend of a rabbit and a squirrel, has soft blue fur and a bushy, striped tail. It hops along a sparkling stream, its eyes wide with wonder. The forest is alive with magical elements: flowers that glow and change colors, trees with leaves in shades of purple and silver, and small floating lights that resemble fireflies. The creature stops to interact playfully with a group of tiny, fairy-like beings dancing around a mushroom ring. The creature looks up in awe at a large, glowing tree that seems to be the heart of the forest.")
     args = parser.parse_args()
 
     assert (args.num_frames - 1) % 4 == 0, "num_frames should be 4 * k + 1"
